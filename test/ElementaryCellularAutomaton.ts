9b67672ee527f4ce46721e533bded814f6de8d14
William Doyle <willdoyle422@gmail.com>
2023-09-16 10:31:08 -0400

    making basic consistency test

--- test/ElementaryCellularAutomaton.ts
+++ test/ElementaryCellularAutomaton.ts
@@ -1,3 +1,4 @@
+import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("ElementaryCellularAutomaton", function () {
@@ -9,7 +10,7 @@ describe("ElementaryCellularAutomaton", function () {
 
         const show = async () => console.log(await contract.print());
         await show()
-        
+
         await contract.next(30, 10);
         await show()
         console.log("done")
@@ -17,5 +18,25 @@ describe("ElementaryCellularAutomaton", function () {
     });
 
 
+    it.only("Check bitmap is as expected", async function () {
+        const a = await ethers.deployContract("ElementaryCellularAutomaton");
+
+        // check that the bitmap is as expected at a specific index
+        const expectBitmapAtNToBe = async (n: number, expected: number) => expect(await a['bitmap'](n)).to.equal(expected)
+        await expectBitmapAtNToBe(0, 1)
+
+        // check that the index provided is out of bounds
+        const expectIndexToBeOutOfBounds = async (n: number) => expect(a['bitmap'](n)).to.be.revertedWithoutReason()
+        await expectIndexToBeOutOfBounds(1)
+
+        // apply rule 30 1 time
+        await a.next(30, 1)
+        await expectBitmapAtNToBe(0, 1)
+        await expectBitmapAtNToBe(1, 1)
+        await expectBitmapAtNToBe(2, 1)
+        await expectIndexToBeOutOfBounds(3)
+
+
+    });
 
 })
