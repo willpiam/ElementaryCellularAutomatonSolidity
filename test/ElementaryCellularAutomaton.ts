42f4979f47b546fdc56b1b2379098a2c131aef6a
william_doyle <mcbidebilly@gmail.com>
2023-09-18 18:20:13 -0400

    test shows that gas estimate does go up with the size of the bitmap

--- test/ElementaryCellularAutomaton.ts
+++ test/ElementaryCellularAutomaton.ts
@@ -7,6 +7,7 @@ describe("ElementaryCellularAutomaton", function () {
     it("simple test", async function () {
         const contract = await ethers.deployContract("ElementaryCellularAutomaton", [[1], 1]);
         await contract.next(30, 1);
+
         await contract.next(30, 5);
 
         const show = async () => console.log(await contract.print());
@@ -23,13 +24,13 @@ describe("ElementaryCellularAutomaton", function () {
             }
         }
         const timer = startTimer()
-        await contract.next(30, 16 );
+        await contract.next(30, 16);
         timer()
-        await contract.next(30, 16 );
+        await contract.next(30, 16);
         timer()
-        await contract.next(30, 16 );
+        await contract.next(30, 16);
         timer()
-        await contract.next(30, 8 );
+        await contract.next(30, 8);
         timer()
         console.log("done")
     });
@@ -51,7 +52,7 @@ describe("ElementaryCellularAutomaton", function () {
         }
         await expectBitmapAtNToBe(0, 1)
 
-       
+
         // apply rule 30 1 time
         await a.next(30, 1)
         await expectBitmapAtNToBe(0, 1)
@@ -79,20 +80,41 @@ describe("ElementaryCellularAutomaton", function () {
 
     });
 
-    it.only("Start with an initial bitmap that is just under 256 bits", async function () {
-        // call next() to generate a generation which should require a new word in the bitmap
+    it("Start with an initial bitmap that is just under 256 bits", async function () {
         // initial seed is 255 random bits
         const initialSeed = Array.from({ length: 254 }, () => Math.floor(Math.random() * 2)).join('')
         console.log(`initialSeed: ${initialSeed}`)
         const seedAsBigInt = BigInt(`0b${initialSeed}`)
         const a = await ethers.deployContract("ElementaryCellularAutomaton", [[seedAsBigInt], 254]);
 
-        await a.next(30, 5)
+        // call next a few times such that we cross over the threshold of 256 bits and start writing to the next word
+        {
+            const gasLimit = await a.next.estimateGas(30, 1);
+            console.log(`gasLimit: ${gasLimit}`)
+        }
+        await a.next(30, 1)
+    });
 
-         
+    it.only("Gas estimate depends on current state of the bitmap", async function () {
+        // create a contract with small initial conditions .. 
+        const a = await ethers.deployContract("ElementaryCellularAutomaton", [[1], 1]);
 
-        
+        // .. and another contract with large initial conditions
+        const randomSeed = (_length : number) : string => `0b${Array.from({ length: _length }, () => Math.floor(Math.random() * 2)).join('')}`
+        const b = await ethers.deployContract("ElementaryCellularAutomaton", [[BigInt(randomSeed(254))], 254]);
 
+        const a_est = await a.next.estimateGas(30, 1);
+        const b_est = await b.next.estimateGas(30, 1);
+
+        console.log(`a_est: ${a_est}`)
+        console.log(`b_est: ${b_est}`)
+
+        console.log(`Prediction b_est will be significantly larger than a_est: ${b_est > a_est}`)
+
+        const c = await ethers.deployContract("ElementaryCellularAutomaton", [[BigInt(randomSeed(256)), BigInt(randomSeed(50))], 306]);
+
+        const c_est = await c.next.estimateGas(30, 1);
+        console.log(`c_est: ${c_est}`)
     });
 
 })
