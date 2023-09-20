02a63d4dd3a8d57e85751ea4d45c51ee382a54f4
william_doyle <mcbidebilly@gmail.com>
2023-09-20 13:37:46 -0400

    begin converting contract from uint256 to uint8

--- contracts/ElementaryCellularAutomaton.sol
+++ contracts/ElementaryCellularAutomaton.sol
@@ -4,17 +4,17 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 // import "hardhat/console.sol";
 
 function setBitIn(
-    uint256[] memory array,
+    uint8[] memory array,
     uint256 _index,
     bool _value
-) pure returns (uint256[] memory) {
-    uint256 wordIndex = _index / 256;
-    uint256 bitIndex = _index % 256;
+) pure returns (uint8[] memory) {
+    uint256 wordIndex = _index / 8;
+    uint8 bitIndex = uint8(_index % 8);
 
     require(array.length > wordIndex, "setBitIn:Index out of bounds");
 
     if (_value) {
-        array[wordIndex] |= (1 << bitIndex);
+        array[wordIndex] |= (uint8(1) << bitIndex);
     } else {
         array[wordIndex] &= ~(1 << bitIndex);
     }
@@ -23,11 +23,11 @@ function setBitIn(
 }
 
 function readBitFrom(
-    uint256[] memory array,
-    uint256 _index
+    uint8[] memory array,
+    uint8 _index
 ) pure returns (bool) {
-    uint256 wordIndex = _index / 256;
-    uint256 bitIndex = _index % 256;
+    uint8 wordIndex = _index / 256;
+    uint8 bitIndex = _index % 256;
 
     require(array.length > wordIndex, "readBitFrom:Index out of bounds");
 
@@ -35,7 +35,7 @@ function readBitFrom(
 }
 
 function calculateNextGenerationCell(
-    uint256[] memory parentCells, // we only read the first 3 bits from this
+    uint8[] memory parentCells, // we only read the first 3 bits from this
     uint8 rule
 ) pure returns (bool) {
     uint8 index = 0;
@@ -54,9 +54,9 @@ function calculateNextGenerationCell(
 }
 
 function _print(
-    uint256 initialGenerationSize,
-    uint256 generationSize,
-    uint256[][] memory history,
+    uint8 initialGenerationSize,
+    uint8 generationSize,
+    uint8[][] memory history,
     string memory empty,
     string memory alive,
     string memory dead
@@ -71,11 +71,11 @@ function _print(
         )
     );
 
-    uint256 initialPadding = (generationSize - initialGenerationSize) / 2;
+    uint8 initialPadding = (generationSize - initialGenerationSize) / 2;
 
-    for (uint256 i = 0; i < history.length; i++) {
+    for (uint8 i = 0; i < history.length; i++) {
         string memory generation = "";
-        for (uint256 k = 1; k <= initialGenerationSize + 2 * i; k++) {
+        for (uint8 k = 1; k <= initialGenerationSize + 2 * i; k++) {
             if (readBitFrom(history[i], (initialGenerationSize + 2 * i) - k)) {
                 generation = string(abi.encodePacked(generation, alive));
             } else {
@@ -84,7 +84,7 @@ function _print(
         }
 
         string memory pad = "";
-        for (uint256 j = 0; j < initialPadding - i; j++) {
+        for (uint8 j = 0; j < initialPadding - i; j++) {
             pad = string(abi.encodePacked(pad, empty));
         }
         generation = string(abi.encodePacked(pad, generation, pad, "\n"));
@@ -95,22 +95,22 @@ function _print(
 }
 
 contract ElementaryCellularAutomaton {
-    uint256 public initialGenerationSize;
-    uint256 public generationSize;
-    uint256[][] public history;
-    uint256[] public bitmap;
+    uint8 public initialGenerationSize;
+    uint8 public generationSize;
+    uint8[][] public history;
+    uint8[] public bitmap;
 
-    constructor(uint256[] memory initialState, uint256 _initialGenerationSize) {
+    constructor(uint8[] memory initialState, uint8 _initialGenerationSize) {
         initialGenerationSize = _initialGenerationSize;
         generationSize = initialGenerationSize;
-        for (uint256 i = 0; i < generationSize; i++) {
+        for (uint8 i = 0; i < generationSize; i++) {
             setBit(i, readBitFrom(initialState, i));
         }
         bitmap.push(0);
     }
 
-    function setBit(uint256 _index, bool _value) internal {
-        uint256 wordIndex = _index / 256;
+    function setBit(uint8 _index, bool _value) internal {
+        uint8 wordIndex = _index / 256;
 
         while (bitmap.length <= wordIndex) {
             bitmap.push(0);
@@ -120,20 +120,20 @@ contract ElementaryCellularAutomaton {
     }
 
     function getBitFromHistory(
-        uint256 historyIndex,
-        uint256 bitIndex
+        uint8 historyIndex,
+        uint8 bitIndex
     ) internal view returns (bool) {
         return readBitFrom(history[historyIndex], bitIndex);
     }
 
     function applyRule(
         uint8 _rule,
-        uint256[] memory previousState
-    ) internal view returns (uint256[] memory) {
-        uint256[] memory nextGeneration = new uint256[](generationSize);
-        uint256[] memory currentGeneration = new uint256[](generationSize);
+        uint8[] memory previousState
+    ) internal view returns (uint8[] memory) {
+        uint8[] memory nextGeneration = new uint8[](generationSize);
+        uint8[] memory currentGeneration = new uint8[](generationSize);
 
-        for (uint256 i = 0; i < previousState.length; i++) {
+        for (uint8 i = 0; i < previousState.length; i++) {
             currentGeneration = setBitIn(
                 currentGeneration,
                 i + 2,
@@ -141,9 +141,9 @@ contract ElementaryCellularAutomaton {
             );
         }
 
-        uint256[] memory parentCells = new uint256[](1); // 1 word is more than enough to store 3 bits
+        uint8[] memory parentCells = new uint8[](1); // 1 word is more than enough to store 3 bits
 
-        for (uint256 i = 0; i < previousState.length + 2; i++) {
+        for (uint8 i = 0; i < previousState.length + 2; i++) {
             parentCells = setBitIn(
                 parentCells,
                 2,
@@ -172,9 +172,9 @@ contract ElementaryCellularAutomaton {
 
     function _next(uint8 _rule) internal {
         history.push(bitmap); // Update history with the current bitmap
-        uint256[] memory currentGeneration = new uint256[](generationSize);
+        uint8[] memory currentGeneration = new uint8[](generationSize);
 
-        for (uint256 i = 0; i < generationSize; i++) {
+        for (uint8 i = 0; i < generationSize; i++) {
             currentGeneration = setBitIn(
                 currentGeneration,
                 i,
@@ -182,17 +182,17 @@ contract ElementaryCellularAutomaton {
             );
         }
 
-        uint256[] memory nextGeneration = applyRule(_rule, currentGeneration);
+        uint8[] memory nextGeneration = applyRule(_rule, currentGeneration);
 
-        for (uint256 i = 0; i < generationSize + 2; i++) {
+        for (uint8 i = 0; i < generationSize + 2; i++) {
             setBit(i, readBitFrom(nextGeneration, i));
         }
 
         generationSize += 2;
     }
 
-    function next(uint8 _rule, uint256 applications) public {
-        for (uint256 k = 0; k < applications; k++) {
+    function next(uint8 _rule, uint8 applications) public {
+        for (uint8 k = 0; k < applications; k++) {
             _next(_rule);
         }
     }
